40c77c0b6210eae685b657bc6312dad85f8d13ff
Gavin Wyant <gavincwyant@gmail.com>
2025-11-11 15:12:20 -0500

    now the simulation tracks the percentage of lost packet as user walks away. much more useful

--- simulations/drone_wifi_simulation.cc
+++ simulations/drone_wifi_simulation.cc
@@ -5,23 +5,58 @@
 #include "ns3/mobility-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/ssid.h"
+#include "ns3/config-store-module.h"
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("DroneWifiSimulation");
 
-int main(int argc, char *argv[]) {
+// Globals for tracking
+uint64_t g_txPackets = 0;
+uint64_t g_rxPackets = 0;
+Ptr<Node> g_user;
+Ptr<Node> g_ap;
+
+// Collect packet Tx/Rx stats
+void TxTrace(Ptr<const Packet> p) { g_txPackets++; }
+void RxTrace(Ptr<const Packet> p, const Address &) { g_rxPackets++; }
+
+// Periodically print network stats
+void Monitor(Time interval)
+{
+  Ptr<MobilityModel> userMob = g_user->GetObject<MobilityModel>();
+  Ptr<MobilityModel> apMob = g_ap->GetObject<MobilityModel>();
+  double distance = userMob->GetDistanceFrom(apMob);
+
+  double lossRate = 0.0;
+  if (g_txPackets > 0)
+    lossRate = 100.0 * (1.0 - (double)g_rxPackets / g_txPackets);
+
+  // Print timestamp, distance, and loss
+  std::cout << Simulator::Now().GetSeconds() << "s: "
+            << "Distance=" << distance << "m, "
+            << "Tx=" << g_txPackets << ", Rx=" << g_rxPackets
+            << " (" << lossRate << "% loss)"
+            << std::endl;
+
+  // Schedule next check
+  Simulator::Schedule(interval, &Monitor, interval);
+}
+
+int main(int argc, char *argv[])
+{
   Time::SetResolution(Time::NS);
   LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
   LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
   NodeContainer baseStation;
   baseStation.Create(1);
-
   NodeContainer user;
   user.Create(1);
+  g_user = user.Get(0);
+  g_ap = baseStation.Get(0);
 
-  // WiFi channel
+  // Channel + PHY
   YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
   YansWifiPhyHelper phy;
   phy.SetChannel(channel.Create());
@@ -31,21 +66,24 @@ int main(int argc, char *argv[]) {
   WifiMacHelper mac;
 
   Ssid ssid("base-ap");
-  mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid), "ActiveProbing", BooleanValue(false));
+  mac.SetType("ns3::StaWifiMac",
+              "Ssid", SsidValue(ssid),
+              "ActiveProbing", BooleanValue(false));
   NetDeviceContainer userDevice = wifi.Install(phy, mac, user);
 
   mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid));
   NetDeviceContainer apDevice = wifi.Install(phy, mac, baseStation);
 
+  // Mobility
   MobilityHelper mobility;
   mobility.SetMobilityModel("ns3::ConstantVelocityMobilityModel");
   mobility.Install(user);
   mobility.Install(baseStation);
 
   user.Get(0)->GetObject<ConstantVelocityMobilityModel>()->SetPosition(Vector(0.0, 0.0, 0.0));
-  user.Get(0)->GetObject<ConstantVelocityMobilityModel>()->SetVelocity(Vector(1.0, 0.0, 0.0)); // move 1 m/s
+  user.Get(0)->GetObject<ConstantVelocityMobilityModel>()->SetVelocity(Vector(5.0, 0.0, 0.0)); // 5 m/s away from spawn
 
-  baseStation.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(0.0, 50.0, 10.0));
+  baseStation.Get(0)->GetObject<MobilityModel>()->SetPosition(Vector(0.0, 0.0, 0.0));
 
   InternetStackHelper stack;
   stack.Install(user);
@@ -70,6 +108,16 @@ int main(int argc, char *argv[]) {
   ApplicationContainer clientApps = echoClient.Install(user.Get(0));
   clientApps.Start(Seconds(2.0));
   clientApps.Stop(Seconds(60.0));
+
+  // Connect traces for packet tracking
+  Ptr<UdpEchoClient> clientApp = DynamicCast<UdpEchoClient>(clientApps.Get(0));
+  Ptr<UdpEchoServer> serverApp = DynamicCast<UdpEchoServer>(serverApps.Get(0));
+
+  clientApp->TraceConnectWithoutContext("Tx", MakeCallback(&TxTrace));
+  serverApp->TraceConnectWithoutContext("Rx", MakeCallback(&RxTrace));
+
+  // Start periodic monitoring
+  Simulator::Schedule(Seconds(2.0), &Monitor, Seconds(2.0));
 
   phy.EnablePcapAll("drone_wifi_simulation");
 
